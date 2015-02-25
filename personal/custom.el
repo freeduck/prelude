c82889868d5986125095417e002a35d69456ce43
Kristian Nygaard Jensen <freeduck@member.fsf.org>
2015-02-25 15:54:12 +0100

    Custom settings

---
+++ personal/custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(bmkp-last-as-first-bookmark-file "/home/kristian/.emacs.d/savefile/bookmarks")
+ '(custom-safe-themes (quote ("c739f435660ca9d9e77312cbb878d5d7fd31e386a7758c982fa54a49ffd47f6e" "f0a99f53cbf7b004ba0c1760aa14fd70f2eabafe4e62a2b3cf5cabae8203113b" default)))
+ '(debug-on-error t)
+ '(syslog-datetime-regexp "^[0-9]\\{4\\}")
+ '(whitespace-style (quote (face tabs empty trailing))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
