6d8562d19e5d9f63d680dcdd512d7fb5442160dd
Kristian Nygaard Jensen <knj@smk.dk>
2015-08-28 13:17:43 +0200

    Configure org

--- personal/400-org.el
+++ personal/400-org.el
@@ -24,22 +24,14 @@
 
 ;;; Code:
 
-(let ((notes-file "~/org/notes.org"))
-  (setq org-agenda-files (list notes-file))
-  (setq org-default-notes-file notes-file))
-
-(define-key global-map "\C-cc" 'org-capture)
-;; Be sure to add your main Org Mode file to your agenda list. You can do so by going to the file and typing C-c [
-
-
+(setq org-agenda-files (list "~/org/tasks"))
+(setq org-default-notes-file "~/org/notes.org")
 
 (setq org-todo-keywords
       '((sequence "TODO(t)" "IN-PROGRESS(i!)" "PAUSE(p@/i)" "|" "DONE(d)" "FORWARD(f)")
         (sequence "|" "CANCELED(c@)")))
 
 (setq org-agenda-todo-list-sublevels nil)
-
-(setq org-refile-targets '((org-agenda-files . (:maxlevel . 6))))
 
 (provide '400-org)
 ;;; 400-org.el ends here
