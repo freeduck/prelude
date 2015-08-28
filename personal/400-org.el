105461a8e5e0b3f2e3c20503de8294479a47311b
Kristian Nygaard Jensen <knj@smk.dk>
2015-08-28 13:17:43 +0200

    Configure org

--- personal/400-org.el
+++ personal/400-org.el
@@ -24,7 +24,7 @@
 
 ;;; Code:
 
-(setq org-agenda-files (list "~/org/tasks.org"))
+(setq org-agenda-files (list "~/org/tasks"))
 (setq org-default-notes-file "~/org/notes.org")
 
 (setq org-todo-keywords
