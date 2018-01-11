735c689a63b8250443e0c51a14d28945ac283e1f
Kristian Nygaard Jensen <freeduck@member.fsf.org>
2018-01-11 10:26:59 +0100

    activate yas for json and terraform

--- personal/300-config.el
+++ personal/300-config.el
@@ -40,6 +40,8 @@
 (when (boundp 'helm-map)
   (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)) ; rebind tab to run persistent action
 
+(add-hook 'json-mode-hook #'yas-minor-mode)
+(add-hook 'terraform-mode-hook #'yas-minor-mode)
 (add-to-list 'load-path "/usr/share/doc/git-core-doc/contrib/emacs")
 (add-to-list 'auto-mode-alist '("\\.j2\\'" . jinja2-mode))
 (when (not (boundp 'eshell-visual-commands))
