bffa446a51bdc6b17e7fc1ad09e76f356ffd21b1
Kristian Nygaard Jensen <freeduck@member.fsf.org>
2020-10-29 23:53:32 +0100

    Enable org babel shell

--- personal/90-org.el
+++ personal/90-org.el
@@ -1,2 +1,12 @@
 (use-package htmlize
   :ensure t)
+
+(use-package org
+  :ensure t
+  :config
+  (org-babel-do-load-languages 'org-babel-load-languages
+                               (append '()
+                                       (if (equal (org-version)
+                                                  "8.2.10")
+                                           '((sh . t))
+                                         '((shell . t))))))
