b0139a269fd06fe320c5db6fe9f9ee443bdddd54
Kristian Nygaard Jensen <kristian.nygaard.jensen@coop.dk>
2021-03-05 10:10:06 +0100

    Use utf8 and unix line endings

---
+++ personal/110-file-coding.el
@@ -0,0 +1,4 @@
+(setq-default buffer-file-coding-system 'utf-8-unix)
+(setq-default default-buffer-file-coding-system 'utf-8-unix)
+(set-default-coding-systems 'utf-8-unix)
+(prefer-coding-system 'utf-8-unix)
