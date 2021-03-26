02b104d82d7e1bffa9b60187a23ac0e74632d526
Kristian Nygaard Jensen <kristian.nygaard.jensen@coop.dk>
2021-03-26 09:53:38 +0100

    windows shell

--- personal/00-init.el
+++ personal/00-init.el
@@ -2,3 +2,8 @@
 (require 'tramp)
 (add-to-list 'tramp-remote-path 'tramp-own-remote-path)
 (filesets-init)
+
+;; (setq explicit-shell-file-name
+;;       "c:/Windows/System32/bash.exe")
+;; (setq shell-file-name explicit-shell-file-name)
+;; (add-to-list 'exec-path "c:/Windows/System32/")
